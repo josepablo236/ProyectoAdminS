b5d5ad8a49ad84cb40e3f41f8ea7e9bb2a877037
josepablo236 <jose.oreo99@gmail.com>
2021-09-21 22:19:23 -0600

    Altura de imagenes

--- proyecto/src/components/principal/Informacion.js
+++ proyecto/src/components/principal/Informacion.js
@@ -9,7 +9,7 @@ const Informacion = () => {
             <div class="row">
                 <div class="col-sm-6 col-md-4">
                     <div className="card">
-                        <img src={playa} class="card-img-top" alt="..." width="100px"/>
+                        <img src={playa} class="card-img-top" alt="..." height="230px"/>
                         <div className="card-body">
                             <h1 className="class-Title">¿Pensando en descansar?</h1>
                             <p className="card-text">Muestranos tu lugar ideal para viajar y lo buscaremos para ti.</p>
@@ -18,7 +18,7 @@ const Informacion = () => {
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div className="card">
-                        <img src={avion} class="card-img-top" alt="..." height="200px"/>
+                        <img src={avion} class="card-img-top" alt="..." height="230px"/>
                         <div className="card-body">
                             <h1 className="class-Title">¿No sabes en donde queda?</h1>
                             <p className="card-text">Te mostramos la ubicación exacta para que viajes a tu lugar de destino.</p>
@@ -27,7 +27,7 @@ const Informacion = () => {
                 </div>
                 <div class="col-sm-6 col-md-4">
                     <div className="card">
-                        <img src={lago} class="card-img-top" alt="..." height="200px"/>
+                        <img src={lago} class="card-img-top" alt="..." height="230px"/>
                         <div className="card-body">
                             <h1 className="class-Title">¿No sabes qué ropa llevar?</h1>
                             <p className="card-text">Te recomendamos ropa adecuada según el clima de tu destino.</p>
