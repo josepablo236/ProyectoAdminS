65a5f267ea9cb926c7f6793e07056dedc9ed7fb8
acamara22 <acamara0999@gmail.com>
2021-10-22 22:36:32 -0600

    correo

--- proyecto/src/components/resultado/Form.js
+++ proyecto/src/components/resultado/Form.js
@@ -1,10 +1,14 @@
-import React, {useContext, useEffect, useState, Fragment} from 'react';
+import React, {useContext, useEffect, useState, Fragment, useRef} from 'react';
 import {ImagenContext} from '../../context/ImagenContext';
 import { evaluarTemperatura } from '../../helpers';
 import axios from 'axios';
-const Form = () => {
-    const { ubicacion, user, clima, errorclima, retro, base, guardarBase} = useContext(ImagenContext);
+import { Print } from '@material-ui/icons';
+import emailjs from 'emailjs-com';
+//import { FacebookButton, FacebookCount } from "react-social";
 
+const Form = () => {
+    const { ubicacion, user, clima, retro, enviar, base, guardarBase, guardarEnviar } = useContext(ImagenContext);
+    const resultadoInicial = " a ver";
     //State para enviar
     const [enviado, guardarEnviado] = useState(false);
 
@@ -17,8 +21,8 @@ const Form = () => {
             email: user.correo,
             nombre: user.nombre,
             lugar: clima.nombre,
-            lat: ubicacion.latitud.toString(),
-            lng: ubicacion.longitud.toString(),
+            lat: ubicacion.latitud,
+            lng: ubicacion.longitud,
             direccion: ubicacion.direccion,
             clima: objeto.estado,
             temp: clima.temp,
@@ -26,13 +30,22 @@ const Form = () => {
             temp_min: clima.temp_min,
             retro: retro
         });
+        guardarEnviar(true);
         var resultado = `Hola ${user.nombre}, el resultado de tu busqueda fue: 
-        Lugar: ${clima.nombre}, Ubicación: lat: ${ubicacion.latitud.toString()} lng: ${ubicacion.longitud.toString()} 
+        Lugar: ${clima.nombre}, Ubicación: lat: ${ubicacion.latitud} lng: ${ubicacion.longitud} 
         Clima: ${objeto.estado} Temperatura: ${clima.temp} Temperatura Max: ${clima.temp_max} Temperatura min: ${clima.temp_min} 
         ${objeto.parrafo} Prendas para usar: ${objeto.prendas}`;
         console.log(resultado);
+
         guardarEnviado(true);
+        emailjs.sendForm('service_MisProximas', 'template_MisProximas', form.current, 'user_VRWnWCPNstyie9BfagJ8v')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
     }
+    const form = useRef(null);
     //Meter el resultado a la base de datos
     useEffect(() => {
         const enviarDatos = async () =>{
@@ -42,49 +55,49 @@ const Form = () => {
                 if(response.status === 200)
                 {
                     console.log("Se guardó el resultado", base);
-                    console.log(response);
                 }
                 else{
                     console.log("No se pudo guardar el resultado");
                 }
             })
         }
-        if(enviado){
+        if(enviar){
             enviarDatos();
+            
         }
-    }, [enviado]);
+    }, [enviar]);
     return ( 
-        <Fragment>
+        <div className="container card mt-3 mb-5">
             {
-                (!errorclima)
+                (!enviado)
                 ?
-                    <div className="container card mt-3 mb-5">
-                        {
-                            (!enviado)
-                            ?
-                            <Fragment>
-                                <h2 className="text-center mt-3 mb-3 fs-4">Agrega tu correo para enviarte los resultados</h2>
-                                <form
-                                    onSubmit={enviarCorreo}
-                                    className="mb-3"
-                                >
-                                    <div className="row mb-3">
-                                        <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-                                        <div className="col-sm-10">
-                                        <input type="email" className="form-control" id="inputEmail3"/>
-                                        </div>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Enviar</button>
-                                </form>
-                            </Fragment>
-                            :
-                            <h2 className="text-center mt-3 mb-3 fs-4">¡Te hemos enviado la información!</h2>
-                        }
-                    </div>
+                <Fragment>
+                    <h2 className="text-center mt-3 mb-3 fs-4">Agrega tu correo para enviarte los resultados</h2>
+                    <form ref={form}
+                        onSubmit={enviarCorreo}
+                        className="mb-3"
+                    >
+                        <div className = "row pt-5 mx-auto">
+                            <div className = "col-8 form-group mx-auto">
+                                <input type = "text" className="from-control" placeholder = "Name" name = "to_name" value = {user.nombre}/>
+                            </div>
+                            <div className = "col-8 form-group mx-auto">
+                                <input type = "text" className="from-control" placeholder = "Email" name = "email"/>
+                            </div>
+                            <div className = "col-8 form-group mx-auto">
+                                <input type = "text" className="from-control" placeholder = "Message" name = "message" value={`Hola ${user.nombre}, el resultado de tu busqueda fue: 
+                                                 Lugar: ${clima.nombre}, Ubicación: lat: ${ubicacion.latitud} lng: ${ubicacion.longitud} 
+                                                 Clima: ${objeto.estado} Temperatura: ${clima.temp} Temperatura Max: ${clima.temp_max} Temperatura min: ${clima.temp_min} 
+                                                 ${objeto.parrafo} Prendas para usar: ${objeto.prendas}`}/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+                </Fragment>
                 :
-                    null
+                <h2 className="text-center mt-3 mb-3 fs-4">¡Te hemos enviado la información!</h2>
             }
-        </Fragment>
+        </div>
      );
 }
  
